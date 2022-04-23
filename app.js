09a86df200931f0ad2ac348715141b429897e613
Somil Agrawal <somilagrawal680@gmail.com>
2022-04-23 21:17:17 +0530

    feat: update

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const date = document.querySelector('#input-date');
-const number = document.querySelector('input-number');
+const input = document.querySelector('#input-number');
 const btn = document.querySelector('#btn');
 const output = document.querySelector('#outputBox');
- 
+
+
 btn.addEventListener('click' , luckynumber);
 console.log("clicked");
+
 function luckynumber(){
-    console.log('click');
+    const dob =(date.value);
+    const number = input.value
+    console.log(dob)
+    const sum = calculateSum(dob)
+    console.log(sum);
+    // const total = sum % (number) ;
+    // console.log(total)
+    if(sum % number === 0){
+        console.log('Your birthday is lucky')
+        output.innerHTML = `Your birthday is Lucky`;
+    }else{
+        console.log("Your birthday is not lucky so sad")
+        output.innerHTML = `Your birthday is not Lucky , So sad`;
+    }
+}
+function calculateSum(dob){
+    dob = (dob.replaceAll('-' , '') );
+    console.log(dob)
+    let sum = 0;
+    for(let digit of dob ) {
+        sum = sum + Number(digit)  ;  
+    };
+    return sum;
 }
