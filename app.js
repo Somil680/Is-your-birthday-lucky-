f3aeb2dabcc8b12481ababbef2daa887ee785485
Somil Agrawal <somilagrawal680@gmail.com>
2022-04-23 11:41:42 +0530

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+const date = document.querySelector('#input-date');
+const number = document.querySelector('input-number');
+const btn = document.querySelector('#btn');
+const output = document.querySelector('#outputBox');
+ 
+btn.addEventListener('click' , luckynumber);
+console.log("clicked");
+function luckynumber(){
+    console.log('click');
+}
